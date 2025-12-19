a49531b72a70e187c3c6699a0bd0cb25387448ca
Yixiao <yixiao.zhang@epfl.ch>
2025-12-19 17:09:29 +0100

    Fix 911 bug

--- assets/js/chartPathUtils.js
+++ assets/js/chartPathUtils.js
@@ -5,7 +5,7 @@ const BASE_PATH = './assets/img/plots';
 
 // Some images use T0-1 dates; map overrides where filenames differ from event.start
 const DATE_OVERRIDES = {
-  1: '2001-09-10', // 9/11 event files are stamped 09-10
+  1: '2001-09-11', // 9/11 event files are stamped 09-10
   6: '2020-03-09'  // COVID files use 03-09 for drawdown/volcano
 };
 
@@ -45,4 +45,13 @@ export async function getChartPaths(eventId, sectorKey) {
     drawdown: `${BASE_PATH}/chapter1/drawdown_curve_plots/drawdown_charts_${sectorFolder}/${otherRef}.png`,
     volcano: `${BASE_PATH}/chapter1/volcano_plots/volcano_charts_${sectorFolder}/volcano_${otherRef}.png`
   };
+}
+
+export async function getSectorHeatmapPath(eventId) {
+  const event = await getEventById(eventId);
+  if (!event) return '';
+  const padded = String(event.id).padStart(3, '0');
+  const date = DATE_OVERRIDES[event.id] || event.start;
+  const ref = `E${padded}_${date}`;
+  return `${BASE_PATH}/chapter2/Sector_Evolution_Heatmaps/${ref}_evolution.png`;
 }
